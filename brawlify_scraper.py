d756a275db0d013487f8e3a830a1d8eec3dc543b
Haris Muhammad <harismuhammad@Hariss-MacBook-Pro.local>
2021-06-24 23:24:22 -0600

    Excel file now has headers centered automatically and the font they possess is bolded

--- brawlify_scraper.py
+++ brawlify_scraper.py
@@ -1,14 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
-from openpyxl import load_workbook
+from openpyxl.workbook import Workbook
+from openpyxl.styles import Font
+from openpyxl.styles import Alignment
 
 source = requests.get('https://brawlify.com/league/').text
 soup = BeautifulSoup(source, 'lxml')
 
-wb = load_workbook('Power_League_Stats.xlsx')
+wb = Workbook()
 ws = wb.active
 ws.title = "Power League Stats"
 ws.append(['Brawler', 'Win Rate', 'Map', 'Map Type'])
+
+for header_cell_tuple in ws['A1:D1']:
+    for header_cell in header_cell_tuple:
+        header_cell.font = Font(size=14, bold=True)
+        header_cell.alignment = Alignment(horizontal='center')
 
 for map_info in soup.findAll('div', class_="row event-recommendation justify-content-center align-content-center"):
     for brawler_info in map_info.findAll('a', class_="link event-brl event-brl-img opacity mb-1 mx-1"):
